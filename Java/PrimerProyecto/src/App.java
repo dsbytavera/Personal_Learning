32a6ed5159b2e6d65d5e3227bca6f5959d1b6813
Alberto Tavera <atavera.galicia@gmail.com>
2023-06-06 13:36:05 -0600

    Cambio en el mensaje

--- Java/PrimerProyecto/src/App.java
+++ Java/PrimerProyecto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo!");
+        System.out.println("Que buena está esta Soda-Mate");
     }
 }
