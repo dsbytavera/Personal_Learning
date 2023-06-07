df06d1553ab98932f0ed873dd7144ba2e4091e33
Alberto Tavera <atavera.galicia@gmail.com>
2023-06-07 16:09:45 -0600

    Primer commit

---
+++ Java/ejercicioMayorMenor/src/App.java
@@ -0,0 +1,19 @@
+// El presente ejercicio nos pide resolver lo siguiente:
+// Arma un programa que permita cargar 3 numeros y mostrar cual es el mayor y cual es el menor"
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(capturarTeclado);
+
+        
+        System.out.println("Hello, World!");
+    }
+}
