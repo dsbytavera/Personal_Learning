d0388da5c717da887e1ea8de55ee8472d696347c
Alberto Tavera <atavera.galicia@gmail.com>
2023-06-06 19:51:52 -0600

    se cambio lo que imprime

--- Java/PrimerProyecto/src/App.java
+++ Java/PrimerProyecto/src/App.java
@@ -1,5 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Que buena está esta Soda-Mate");
+    public static void main(String[] args) {
+             String Marca = "Taragüi";
+             System.out.println(Marca);
     }
 }
